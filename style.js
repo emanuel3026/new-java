02cab309cd34a91964d9569b57ac7dc9e9b540bb
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-03-30 09:33:29 -0300

    float

--- style.js
+++ style.js
@@ -1,9 +1,14 @@
 //tipo number
-
+//inteiros ou com pontos flutuantes (float = real)
 const meuNumero = 3; //constante não altera
 console.log (meuNumero); //apresentação
 
-const primeiroNumero = 1;
+const primeiroNumero = 10;
 const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero + segundoNumero;
 console.log("Resultado: ", operacaoMatematica);
+
+
+//ponto flutuante
+const numeroFlutuante = 3.3;
+console.log();
