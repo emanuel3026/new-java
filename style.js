ec6feeff2a7f3979443be700ebfac89fa1780b0b
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-03-30 10:19:31 -0300

    Nan

--- style.js
+++ style.js
@@ -8,7 +8,14 @@ const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero + segundoNumero;
 console.log("Resultado: ", operacaoMatematica);
 
+
 //ponto flutuante
 const numeroFlutuante = 3.3;
 const pontoFlutuanteSemZero = .5;
-console.log();
+
+const novaOperacao = primeiroNumero / numeroFlutuante;
+console.log(novaOperacao.toFixed(2)); // .toFixed permite formatar o número de casa decimais da saida do sistema.
+
+//NaN = Not a Number
+const nome = "celv";
+console.log(nome * primeiroNumero);
