f353300127408303c5c0754ee6b850221a295fcc
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-06 09:03:49 -0300

    arrumando a senha

---
+++ a2-type-string.js
@@ -0,0 +1,9 @@
+//tipo string = texto
+
+const texto1 = "Olá Mundo!";
+const nome = "Emanuel";
+const senha = "1234";
+const stringNumerica = "senhaSuperSegura456"
+console.log(texto1, "\n");
+console.log(nome);
+console.log( "Senha:", senha);
