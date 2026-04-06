ef4a86dd7a20592541555ba04e1a30bb381095c4
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-06 10:12:29 -0300

    boolen

--- a3-type-boolean.js
+++ a3-type-boolean.js
@@ -3,3 +3,15 @@
 const primeiroNumero = 5;
 let segundoNumero = "5";
 console.log(primeiroNumero == segundoNumero)
+
+// 1 = atribui
+// 2 == compara se os valores são iguais, independentes de tipo
+// se for sting com número, faz a conversão.
+
+console.log(primeiroNumero === segundoNumero);
+// 3 === comparação dos valores e tipos de dados
+
+const text1 = "Celv"
+const text2 = "celv"
+console.log(text1 === text2) //case sensitive = diferenciar minisculas de maiusculas
+
