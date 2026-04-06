457ba6fba2a3e2cb21e16e6ba01e2981f07731a6
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-06 10:33:12 -0300

    let

--- a4-script.js
+++ a4-script.js
@@ -10,6 +10,10 @@ var comprimento = 7;
 area = altura * comprimento;
 console.log(area);
 
+var area; // o js vai ler o programa e vai declarar a variavel antes de executar o programa,
+// por isso pode ser declacara depois de ser utilizada.
+console.log(area);
 
+// let = variavel local, funciona dentro do escopo definido, dentro de uma estrutura, por exemplo {}.
 
-//let idade = 15;
+let idade = 15;
