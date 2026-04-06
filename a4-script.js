8d4e9aec82cc4db6b8ec669c55eebb9062734b4a
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-06 10:23:30 -0300

    var

---
+++ a4-script.js
@@ -0,0 +1,15 @@
+// const = um valor que após ser declarado, não pode mais ser alterado.
+
+// var = declaração de variaveis o valor pode ser alterado a qualquer momento.
+// var global = funciona em qualquer parte do codigo.
+
+var idade = 15;
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+
+
+
+//let idade = 15;
