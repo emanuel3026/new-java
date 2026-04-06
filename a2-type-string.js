b891e9d472495dacca5359423775e57b42f88278
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-06 09:15:05 -0300

    citaçao

--- a2-type-string.js
+++ a2-type-string.js
@@ -7,3 +7,6 @@ const stringNumerica = "senhaSuperSegura456"
 console.log(texto1, "\n");
 console.log(nome);
 console.log( "Senha:", senha);
+
+const citacao = "Meu nome é ";
+console.log(citacao + nome);
