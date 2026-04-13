3922225f8f1badab3e4ab7758a6d7e2352bb3a6c
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-13 10:33:40 -0300

    a6

--- a5-truthy-falsy.js
+++ a5-truthy-falsy.js
@@ -5,6 +5,21 @@ const contaPaga = false;
 // 0 = false 
 // 1 = true
 
-console.log(0 == false)
+console.log(0 == false);  //true
+console.log("" == false); //true
+console.log(1 == true);   //true
 
+// null - tipo de dado que representa o vazio
+// indefined - var declarada sem valor
+
+var teste;
+let varNull = null;
+
+console.log(teste);
+console.log(varNull);
+
+console.log(typeof teste);
+console.log(typeof varNull); //aparece como objeto por ser um bug do js, deveria, na verdade, ser null.
+
+console.log(typeof contaPaga);
 
