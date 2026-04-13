59be868aa788fc007aabd8c2a29fd38afa37f750
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-13 10:42:16 -0300

    a6 string

---
+++ a6-conversoes.js
@@ -0,0 +1,12 @@
+// Conversões Tipos de dados
+// Number - int ou float
+// String - texto ou char
+// Boolean = logico verdadeiro ou falso
+
+//conversões Implicitas
+
+const numero = 456;
+const numString = "456";
+
+
+//conversões Explicitas
