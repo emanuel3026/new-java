e22d3732a76e4b6e478a0a2badf11bf88d9d2170
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-23 08:32:57 -0300

    Multiparadigma

---
+++ a7-js-e-node.js
@@ -0,0 +1,12 @@
+// Tipagem dinamica: é você não precisar explicar qual tipo que a variavel é, Exemplo const int numero = 26 . Isso não é tipagem dinamica, 
+// tipagem dunamica é , Exemplo: const numero = 26 isso e tipagem dinamica em nenhum momento eu declarei se isso e um número ou um texto.
+// não precisa definir o tipo de dado que cada variavel ou constante terá.
+
+var num = "5";
+var ligado = 1;
+ligado = "569";
+
+// JavaScript
+// Linguagem Multiparadigma - resolvendo o mesmo problema utilizando diferentes abordagens.
+
+
