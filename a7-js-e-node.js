0571e0429657d3195f15f0644f5e767d55a1286c
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-23 08:47:22 -0300

    Deno

--- a7-js-e-node.js
+++ a7-js-e-node.js
@@ -9,4 +9,16 @@ ligado = "569";
 // JavaScript
 // Linguagem Multiparadigma - resolvendo o mesmo problema utilizando diferentes abordagens.
 
+//ES6 /ES332015 - versão do JS lançada em 2015 com atualizações constantes
 
+//linguagem interpretada - vai analizando linha a linha diferente da linguagem compilada que gera 1 arquivo de maquina para executar. É mais rapida.
+//NodeJS - não é uma linguagem (é o js)
+//Interpretador de JS;
+//o JS, por ser uma linguagem interpretada, precisa de um interpretador
+
+//podemos utilar o navegador como interpretador
+
+//NodeJS é uma ferramenta uqe foi desenvolvida em 2010, bem depois que o javaScript foi lançado.
+
+//NodeJS norlmalmente é utilizado no back-and.
+//Deno é alternativo ao NodeJS.
