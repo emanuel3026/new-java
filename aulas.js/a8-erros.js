30385ee29409d24d8a33b4b664a6135a78e6fb60
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-04-23 12:09:06 -0300

    a8

---
+++ aulas.js/a8-erros.js
@@ -0,0 +1,20 @@
+//const numero; //uma cont não pode ser cirada sem um valor
+/* Syntax/error: na maior parte dos casos JavaScript não consegue executa-lo. Os erros podem ser métodos 
+ou propriedades escritos ou utilizados de forma incorreta, por exemplo, operadores ou sinais gráficos com
+elementos a menos, como esquecer de fechar chaves ou couchetes*/
+
+const numeros=1;
+console.log (numeros);
+  
+console.log (minhaVar);
+
+/*ReferenceError: Normalmente ocorre quando o código tenta acessar algo que não foi definida; 
+muitas vezes é causado por erros de digitação ou confusão nos nomes utilizados, mas támbem pode indicar um erro no programa*/
+
+/* outros tipos de erros:
+RangerError: quando o código recebe um dado do tipo certo, porém não está no formato aceitaveis.
+Por exemplo: um processamento que só aceita valores inteiros positivos, mas recebe -1.*/
+
+/*TypeError: indica que o código esperava receber um código de um determinado tipo,
+tal qual uma string de texto, mas recebeu outro, como um número booleano ou null.*/
+
