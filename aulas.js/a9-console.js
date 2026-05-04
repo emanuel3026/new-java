34f31ad4998076bdaaed610f3978301c8ace80bf
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-05-04 08:06:11 -0300

    outros metodos de tratamento de erro

--- aulas.js/a9-console.js
+++ aulas.js/a9-console.js
@@ -2,3 +2,23 @@
 Console:é uma ferramenta que pode ser encontrada nos navegadores e no NodeJS
 Log é um registro
 */
+
+const minhaVar = true;
+
+console.log("Eu sou um texto");
+console.log(245);
+console.log(minhaVar);
+
+console.log("Minha idade é " + 41); //Concatenação --------- Provavelmente na prova ---------
+
+//tratamento de erro
+console.log("Deu erro");
+
+/*existem outros metodos de tratamento de erro com console:
+console.error(); para exibir mensegens de erro;
+console.table(); para visualizar de forma mais organizada informaçoes tabulares
+console.time(); periodo que uma operaçao de codigo leva para se iniciar e concluida.
+console.trace(); para exibir a atacktrace de todos os ( ou seja, chamados) por onde o codigo executado passou durante a execução.
+*/
+
+console.error("&rr0 3£");
