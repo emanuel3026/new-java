5101f0a088aede0011daa234079d9fc59997cc72
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-05-04 08:51:25 -0300

    carteira de motorista

---
+++ aulas.js/a11-ternario.js
@@ -0,0 +1,16 @@
+//operador ternario uma forma simplificada de criação de estruturas condicionais. if...else
+
+const idadeMin = 18;
+const idadeDoCliente = 19;
+
+// estrutura de decisao condicional
+if (idadeDoCliente >= idadeMin){
+
+        console.log("O seu cliente tem a idade igual ou maior do que 18 anos");
+        console.log("Você pode fazer a carteira de motorista")
+
+}else{
+
+        console.log("você não tem mais doque 18 anos");
+
+}
