e6d686cdd8d6e524cc54832a8ad18a5ecf0b8977
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-05-21 12:18:10 -0300

    a11

--- aulas.js/a11-ternario.js
+++ aulas.js/a11-ternario.js
@@ -1,4 +1,4 @@
-//operador ternario uma forma simplificada de criação de estruturas condicionais. if...else
+/*//operador ternario uma forma simplificada de criação de estruturas condicionais. if...else
 
 const idadeMin = 18;
 const idadeDoCliente = 19;
@@ -15,10 +15,27 @@ if (idadeDoCliente >= idadeMin){
 
 }
 
-if(idadeMin >= idadeDoCliente){
+if(idadeDoCliente >= idadeMin){
 
         console.log("Você pode fazer a carteira de habilitação");
 
 }else{
-        console.log("Você não tem 18 anos, você não pode fazer a cartira de habilitação")
+        console.log("Você não tem 18 anos, você não pode fazer a cartira de habilitação");
 }
+
+//Operador ternàrio
+let permissao = idadeDoCliente >= idadeMin ? "sim" : "não" ;
+console.log("permissão para direigir: ", permissao);
+
+//outro exemplo
+// condição ? resposta V : resposta F;
+// emanuel augusto begnini
+//júlio Cezar lourenço Valmorbida
+//digitar sem olhar quando você decora o teclado fica bem mais fácil 
+let selecao = "Brasil";
+console.log(selecao == "Brasil" ? "O hexa é nosso" : " fica pra proxima");*/
+
+var itens = 3;
+let preco = itens > 3 ? 4.00 : 5.00;
+
+console.log("Valor da compra é: " + itens * preco);
