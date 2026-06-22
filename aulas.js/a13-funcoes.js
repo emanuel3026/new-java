fd0a016d69a8638cde3235cf3cdffe649940fc8c
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-06-22 12:16:27 -0300

    a13 calcular 2

--- aulas.js/a13-funcoes.js
+++ aulas.js/a13-funcoes.js
@@ -12,3 +12,20 @@ function ImpremeTexto(texto){
 //Execução da função
 
 ImpremeTexto(5);
+
+function calcular(soma){
+
+   // console.log(soma);
+   ImpremeTexto(soma);
+    
+}
+
+calcular(2+2);
+
+//Criar uma função calcular que receba 3 
+//parametros: dois numero/valores e a operação(+, -, *, /)
+
+function calcular(){
+
+
+}
