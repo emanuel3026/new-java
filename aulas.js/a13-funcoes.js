19029b4eccb3b34bc9e28d2b9951805dea55ddf1
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-06-29 07:36:48 -0300

    a13 06

--- aulas.js/a13-funcoes.js
+++ aulas.js/a13-funcoes.js
@@ -23,9 +23,37 @@ function calcular(soma){
 calcular(2+2);
 
 //Criar uma função calcular que receba 3 
-//parametros: dois numero/valores e a operação(+, -, *, /)
 
-function calcular(){
+
+function calcular(op, valor1, valor2 ){
+
+    if(op == "+"){
+
+        resultado = valor1 + valor2;
+        console.log(resultado)
+        
+    }
+    if(op == "-"){
+
+        resultado = valor1 - valor2;
+        console.log(resultado)
+        
+    }
+    if(op == "*"){
+
+        resultado = valor1 * valor2;
+        console.log(resultado)
+        
+    }
+    if(op == "/"){
+
+        resultado = valor1 / valor2;
+        console.log(resultado)
+        
+    }
 
 
 }
+
+calcular(3,4,"+");
+
