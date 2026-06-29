f331311f4c8d4e124327dfc3ef87732e8bafe575
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-06-29 07:59:48 -0300

    a13 function

--- aulas.js/a13-funcoes.js
+++ aulas.js/a13-funcoes.js
@@ -25,35 +25,43 @@ calcular(2+2);
 //Criar uma função calcular que receba 3 
 
 
-function calcular(op, valor1, valor2 ){
+/*function calcular( valor1, valor2, op ){
 
     if(op == "+"){
 
         resultado = valor1 + valor2;
-        console.log(resultado)
         
     }
     if(op == "-"){
 
         resultado = valor1 - valor2;
-        console.log(resultado)
-        
+           
     }
     if(op == "*"){
 
         resultado = valor1 * valor2;
-        console.log(resultado)
         
     }
     if(op == "/"){
 
         resultado = valor1 / valor2;
-        console.log(resultado)
         
     }
+
+    console.log(resultado)
 
 
 }
 
-calcular(3,4,"+");
+calcular(3,4,'+');*/
 
+function idade(dia, mes, ano){
+
+    return 2026-ano;
+
+}
+ImpremeTexto(idade(27,10,2002));
+resposta = idade(27,10,2002);
+
+carteiraDeMotorista = resposta - 18;
+console.log('Tem CNH há '+ carteiraDeMotorista + ' anos ')
