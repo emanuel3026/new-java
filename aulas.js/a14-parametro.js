7054bc054376611698e11094785f2341458c3b09
Kauan1243 <bueno.santos.kauan@escola.pr.gov.br>
2026-08-06 10:32:18 -0300

    po

---
+++ aulas.js/a14-parametro.js
@@ -0,0 +1,20 @@
+/*function verificavirus(arq, fonte){//parametro
+
+
+
+}
+function soma(numero1, numero2){
+
+    return
+
+}
+console.log(soma(10,15))*/
+
+function nomeIdade(nome, idade){
+
+    return `Meu nome é ${nome} e minha idade é ${idade}`;
+
+}
+
+texto = nomeIdade("Emanuel", 16);
+//console.log(texto);
